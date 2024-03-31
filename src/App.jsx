f4b1f705762bb40373878e9aef98ec940b290a5b
Samuel Prandi <prandisamuel99@gmail.com>
2024-03-31 14:46:25 +0200

    concluso stile e adattamento desktop. Aggiunto prettierrc per plugin tailwindCss

--- src/App.jsx
+++ src/App.jsx
@@ -2,115 +2,135 @@ import "./App.css";
 
 function App() {
   return (
-    <main>
-      <div>
+    <main className="m-auto grid gap-y-9 bg-White md:my-32 md:max-w-desktop md:rounded-3xl md:p-10 md:pb-6">
+      <div className="truncate md:rounded-xl">
         <img src="/images/image-omelette.jpeg" alt="food picture" />
       </div>
 
-      <div className="p-8 grid gap-y-8">
+      <div className="grid gap-y-8 px-8 md:px-0">
         <div className="grid gap-y-6">
-          <h1 className="font-Youngserif text-4xl text-DarkCharcoal">
+          <h1 className="font-Youngserif text-4xl text-DarkCharcoal md:text-[2.5rem]">
             Simple Omelette Recipe
           </h1>
-          <p className="font-Outfit text-wengeBrown opacity-60">
+          <p className="font-Outfit text-WengeBrown">
             An easy and quick dish, perfect for any meal. This classic omelette
             combines beaten eggs cooked to perfection, optionally filled with
             your choice of cheese, vegetables, or meats.
           </p>
         </div>
 
-        <div className="bg-RoseWhite p-6 rounded-2xl font-Outfit grid gap-y-3">
-          <h2 className="font-OutfitBold text-DarkRaspberry text-xl">Preparation time</h2>
-          <ul className="list-disc px-5 text-wengeBrown opacity-60">
-            <li className="px-5">
-              <b>Total</b>: Approximately 10 minutes
+        <div className="grid gap-y-3 rounded-xl bg-RoseWhite px-6 py-4 font-Outfit md:px-7 md:py-5">
+          <h2 className="text-xl font-OutfitBold text-DarkRaspberry">
+            Preparation time
+          </h2>
+          <ul className="grid list-disc gap-y-1 text-WengeBrown">
+            <li className="flex items-center before:px-2 before:pe-7 before:text-xl before:content-['•']">
+              <p>
+                <b>Total</b>: Approximately 10 minutes
+              </p>
             </li>
-            <li className="px-5">
-              <b>Preparation</b>: 5 minutes
+            <li className="flex items-center before:px-2 before:pe-7 before:text-xl before:content-['•']">
+              <p>
+                <b>Preparation</b>: 5 minutes
+              </p>
             </li>
-            <li className="px-5">
-              <b>Cooking</b>: 5 minutes
+            <li className="flex items-center before:px-2 before:pe-7 before:text-xl before:content-['•']">
+              <p>
+                <b>Cooking</b>: 5 minutes
+              </p>
             </li>
           </ul>
         </div>
 
-        <div className="grid gap-y-6">
-          <h2 className="text-3xl font-Youngserif text-Nutmeg">Ingredients</h2>
-          <ul className="list-disc font-Outfit px-5 text-wengeBrown opacity-60">
-            <li className="px-5">2-3 large eggs</li>
-            <li className="px-5">Salt, to taste</li>
-            <li className="px-5">Pepper, to taste</li>
-            <li className="px-5">1 tablespoon of butter or oil</li>
-            <li className="px-5">
+        <div className="grid gap-y-5">
+          <h2 className="font-Youngserif text-3xl text-[1.75rem] text-Nutmeg">
+            Ingredients
+          </h2>
+          <ul className="grid gap-y-1 px-2 font-Outfit text-WengeBrown">
+            <li className="flex items-center before:pe-7 before:text-xl before:content-['•']">
+              2-3 large eggs
+            </li>
+            <li className="flex items-center before:pe-7 before:text-xl before:content-['•']">
+              Salt, to taste
+            </li>
+            <li className="flex items-center before:pe-7 before:text-xl before:content-['•']">
+              Pepper, to taste
+            </li>
+            <li className="flex items-center before:pe-7 before:text-xl before:content-['•']">
+              1 tablespoon of butter or oil
+            </li>
+            <li className="flex items-center before:pe-7 before:text-xl before:content-['•']">
               Optional fillings: cheese, diced vegetables, cooked meats, herbs
             </li>
           </ul>
         </div>
 
-        <hr className="opacity-15"/>
+        <hr className="border-LightGrey" />
 
-        <div>
-          <h2>Instructions</h2>
-          <ol>
-            <li>
+        <div className="grid gap-y-5 md:gap-y-4">
+          <h2 className="font-Youngserif text-3xl text-[1.75rem] text-Nutmeg">
+            Instructions
+          </h2>
+          <ol className="grid list-decimal gap-y-2 px-6 font-Outfit text-WengeBrown marker:font-Outfit marker:font-OutfitBold marker:text-Nutmeg">
+            <li className="ps-4">
               <b>Beat the eggs</b>: In a bowl, beat the eggs with a pinch of
               salt and pepper until they are well mixed. You can add a
               tablespoon of water or milk for a fluffier texture.
             </li>
-            <li>
+            <li className="ps-4">
               <b>Heat the pan</b>: Place a non-stick frying pan over medium heat
               and add butter or oil.
             </li>
-            <li>
+            <li className="ps-4">
               <b>Cook the omelette</b>: Once the butter is melted and bubbling,
               pour in the eggs. Tilt the pan to ensure the eggs evenly coat the
               surface.
             </li>
-            <li>
+            <li className="ps-4">
               <b>Add fillings (optional)</b>: When the eggs begin to set at the
               edges but are still slightly runny in the middle, sprinkle your
               chosen fillings over one half of the omelette.
             </li>
-            <li>
+            <li className="ps-4">
               <b>Fold and serve</b>: As the omelette continues to cook,
               carefully lift one edge and fold it over the fillings. Let it cook
               for another minute, then slide it onto a plate.
             </li>
-            <li>
+            <li className="ps-4">
               <b>Enjoy</b>: Serve hot, with additional salt and pepper if
               needed.
             </li>
           </ol>
         </div>
 
-        <hr />
+        <hr className="border-LightGrey" />
 
-        <div>
-          <h2>Nutrition</h2>
-          <p>
+        <div className="grid gap-y-3">
+          <h2 className="font-Youngserif text-3xl text-[1.75rem] text-Nutmeg">
+            Nutrition
+          </h2>
+          <p className="font-Outfit text-WengeBrown">
             The table below shows nutritional values per serving without the
             additional fillings.
           </p>
-          <table>
-            <tbody>
-              <tr>
-                <th>Calories</th>
-                <td>277kcal</td>
-              </tr>
-              <tr>
-                <th>Carbs</th>
-                <td>0g</td>
-              </tr>
-              <tr>
-                <th>Protein</th>
-                <td>20g</td>
-              </tr>
-              <tr>
-                <th>Fat</th>
-                <td>22g</td>
-              </tr>
-            </tbody>
-          </table>
+          <ul className="font-Outfit text-WengeBrown">
+            <li className="grid grid-cols-2 gap-x-4 border-b border-LightGrey px-8 py-3">
+              Calories
+              <span className="font-OutfitBold text-Nutmeg">277kcal</span>
+            </li>
+            <li className="grid grid-cols-2 gap-x-4 border-b border-LightGrey px-8 py-3">
+              Carbs
+              <span className="font-OutfitBold text-Nutmeg">0g</span>
+            </li>
+            <li className="grid grid-cols-2 gap-x-4 border-b border-LightGrey px-8 py-3">
+              Protein
+              <span className="font-OutfitBold text-Nutmeg">20g</span>
+            </li>
+            <li className="grid grid-cols-2 gap-x-4 px-8 py-3">
+              Fat
+              <span className="font-OutfitBold text-Nutmeg">22g</span>
+            </li>
+          </ul>
         </div>
       </div>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      "Outfit": ["Outfit", "sans-serif"],
-      "Youngserif": ["YoungSerif", "serif"],
+      Outfit: ["Outfit", "sans-serif"],
+      Youngserif: ["YoungSerif", "serif"],
     },
     fontWeight: {
-      "YoungSerifRegular": 400,
-      "OutfitRegular": 400,
-      "OutfitSemiBold": 600,
-      "OutfitBold": 700
+      YoungSerifRegular: 400,
+      OutfitRegular: 400,
+      OutfitSemiBold: 600,
+      OutfitBold: 700,
     },
     colors: {
-      "Nutmeg": "hsl(14, 45%, 36%)",
-      "DarkRaspberry": "hsl(332, 51%, 32%)",
-      "White": "hsl(0, 0%, 100%)",
-      "RoseWhite": "hsl(330, 100%, 98%)",
-      "Eggshell": "hsl(30, 54%, 90%)",
-      "LightGrey": "hsl(30, 18%, 87%)",
-      "WengeBrown": "hsl(30, 10%, 34%)",
-      "DarkCharcoal": "hsl(24, 5%, 18%)"
-    }
-  }
-}
-
+      Nutmeg: "hsl(14, 45%, 36%)",
+      DarkRaspberry: "hsl(332, 51%, 32%)",
+      White: "hsl(0, 0%, 100%)",
+      RoseWhite: "hsl(330, 100%, 98%)",
+      Eggshell: "hsl(30, 54%, 90%)",
+      LightGrey: "hsl(30, 18%, 87%)",
+      WengeBrown: "hsl(30, 10%, 34%)",
+      DarkCharcoal: "hsl(24, 5%, 18%)",
+    },
+    maxWidth: {
+      desktop: "46rem",
+    },
+  },
+};
